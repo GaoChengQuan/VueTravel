8202d7c97e5a3025344b133209fd4c873cfc008b
GaoChengquan <529390053@qq.com>
2018-04-30 17:27:00 +0800

    自定义路由

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>home</div>
+</template>
+
+<script>
+	export default {
+		name: "Home" //组件的名字
+	}
+</script>
+
+<style>
+	
+	
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>list</div>
+</template>
+
+<script>
+	export default {
+		name: "List" //组件的名字
+	}
+</script>
+
+<style>
+	
+	
+</style>
